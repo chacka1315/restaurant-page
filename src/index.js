61919525248925d5e39b1fc2b391030460fe8710
chacka1315 <chacka131@gmail.com>
2025-07-23 19:17:04 +0000

    I wrote the logic in index.js that allow the user to click on the button and swicth on the page corresponding.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import home from './home.js';
 import menu from './menu.js'
 import contact from './contact.js'
 
-//home();
-//menu();
-contact();
+const content = document.querySelector('.content')
+const homePageBtn = document.querySelector('#homeBtn');
+const menuPageBtn = document.querySelector('#menuBtn');
+const contactPagetBtn = document.querySelector('#contactBtn');
+
+homePageBtn.addEventListener('click', () =>{
+    content.textContent = '';
+    home();
+})
+
+
+menuPageBtn.addEventListener('click', () => {
+    content.textContent = '';
+    menu();
+});
+
+contactPagetBtn.addEventListener('click', () => {
+    content.textContent = '';
+    contact();
+});
+
+home();
+    
